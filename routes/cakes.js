a3cee273027bf6368253684cc8b536d890bad519
beepetaswathi <123126383+beepetaswathi@users.noreply.github.com>
2023-04-19 10:51:03 -0500

    modified

--- routes/cakes.js
+++ routes/cakes.js
@@ -12,5 +12,5 @@ router.get('/create', cakes_controlers.cakes_create_Page);
 /* GET create update page */
 router.get('/update', cakes_controlers.cakes_update_Page);
 
-/* GET delete costume page */
+/* GET delete cakes page */
 router.get('/delete', cakes_controlers.cakes_delete_Page);
