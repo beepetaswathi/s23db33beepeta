f8f1084b61d79eb1f1e908f31dc52045ecb50fda
beepetaswathi <123126383+beepetaswathi@users.noreply.github.com>
2023-04-24 20:31:23 -0500

    modified

--- models/cakes.js
+++ models/cakes.js
@@ -1,7 +1,25 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
 const cakesSchema = mongoose.Schema({
-cake_Name: String,
-cake_flavour: String,
-cost: {type:Number,min:4,max:400}
-})
-module.exports = mongoose.model("cakes", cakesSchema)
+  cake_Name: { 
+    type: String,
+    required: true,
+    unique: true,
+    minLength: [4,"cake Name is not valid "],
+    },
+  cake_flavour: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: [6,"cake flavour is not valid "],
+    },
+  cost: {
+    type: Number,
+    required: true,
+    unique: true,
+    minLength: [7,"cost is not valid "],
+    },
+    
+});
+
+module.exports = mongoose.model("cakes", cakesSchema);
